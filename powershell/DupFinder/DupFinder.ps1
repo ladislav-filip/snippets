001e8315a825250d04787f2a4a775fe3a3220240
LF <ladislav.filip@fullsys.cz>
2018-10-24 17:10:24 +0200

    Skript pro vyhledání duplicit v kódu C# - zatím pouze prototyp

---
+++ powershell/DupFinder/DupFinder.ps1
@@ -0,0 +1,6 @@
+﻿s:\ReSharper-CommandLineTools\dupfinder.exe /output="s:\dupFinder.xml" /show-text "S:\vyvoj\home\net\Imatrade\Imatrade.Registr\Core\ImageManager.cs"
+
+
+$XslPatht = New-Object System.Xml.Xsl.XslCompiledTransform
+$XslPatht.Load("s:\ReSharper-CommandLineTools\dupfinder.xsl")
+$XslPatht.Transform("s:\dupFinder.xml", "s:\dupFinder.html")
